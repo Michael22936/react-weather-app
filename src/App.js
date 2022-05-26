937ea6cf6f32544fed57398b9189bfdcf1dbcd4e
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-05-26 12:27:59 -0500

    finished displaying the location from api

--- src/App.js
+++ src/App.js
@@ -48,8 +48,10 @@ function App() {
 				onKeyPress={search}
 			/>
 		</div>
-		<div className="location-box">
-			<div className="location">New York City, US</div>
+		{(typeof weather.main != "undefined") ? (
+		<div>
+			<div className="location-box">
+			<div className="location">{weather.name}, {weather.sys.country}</div>
 			<div className="date">{dateBuilder(new Date())}</div>
 		</div>
 		<div className="weather-box">
@@ -58,6 +60,8 @@ function App() {
 			</div>
 			<div className="weather">Sunny</div>
 		</div>
+		</div>
+		) : ('')}
       </main>
     </div>
   );
